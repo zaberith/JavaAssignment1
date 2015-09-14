a3756f234e93022d5c1f70ff7c81d3d9013657ef
c0640858 <c0640858@SB-N208-34.lambton.on.ca>
2015-09-14 16:05:47 -0400

    Initial Commit

---
+++ src/javaassignment/pkg1/JavaAssignment1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment.pkg1;
+
+/**
+ *
+ * @author c0640858
+ */
+public class JavaAssignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**********************************
+         * Assignment #1
+         * 
+         * Job Posting:Java Developer
+         * http://www.indeed.ca/viewjob?jk=0401b839b55e9d49&q=Java&tk=19v6l8q5fafivb2j&from=web
+         * 
+         * Java Platform Enterprise Edition: https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * Spring Framework - Application FrameWork - https://en.wikipedia.org/wiki/Spring_Framework
+         * PIG - High Level platform for creating Mapreduce programs -  https://en.wikipedia.org/wiki/Pig_(programming_tool)
+         * 
+         * Salary
+         * Toronto - $78K
+         * Vancouver - $82k
+         * Waterloo - $80k
+         */
+    }
+    
+}
